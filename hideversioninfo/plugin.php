56429568a25b3f6966d037e57c5eba9016513e4f
pfeifferch <73090220+pfeifferch@users.noreply.github.com>
2021-03-03 07:52:04 +0100

    Add files via upload

--- hideversioninfo/plugin.php
+++ hideversioninfo/plugin.php
@@ -1,1 +1,18 @@
+<?php 
+/* 
+Plugin Name: Hide Version String 
+Plugin URI: https://github.com/YOURLS/YOURLS/issues/1878 
+Description: Plugin to hide the version string in the footer. 
+Version: 0.1 
+Author: chtaube 
+Author URI: http://github.com/chtaube 
+*/  
 
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+yourls_add_filter( 'html_footer_text', 'hide_version_string' ); 
+
+function hide_version_string( $value ) { 
+return preg_filter( '/ v .* \&ndash; /', ' &ndash; ', $value );
+}
+/* echo $value."<!-- ttzztt -->";*/
